a8a6dd72fef5bf6b4c7e69db7c6890ff64b1d61a
S.Aziz Khan <sakatazizkhan1@gmail.com>
2024-09-01 17:39:51 +0530

    added dashboard page & layout

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,24 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  console.log({ request });
+  if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("callbackUrl", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
+    "/dashboard/*",
     /*
      * Match all request paths except:
      * - _next/static (static files)
